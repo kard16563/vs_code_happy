bdbffaf80a3a01391c0dba33ae191097859bb14e
kard sacifi <kk20050821@163.com>
2020-02-24 23:30:13 +0800

    2020 2/24 23:29 update input&output 更新输出输出_2

--- c/c_study/s_input&output.cpp
+++ c/c_study/s_input&output.cpp
@@ -1,8 +1,12 @@
 //输入and输出
-#include <stdio.h>
-
+#include<stdio.h>
+//流  是指输入源 键盘..或者任意的输出地址 屏幕...
 
 int main(){
+    FILE *fp1,*fp2;//文件指针
+    //stdin为标准流入 默认为键盘 scanf之类的用它来实现的
+    //stdout为标准流出 默认为屏幕 printf用它写的
+    //stdin ;
 
 
 
