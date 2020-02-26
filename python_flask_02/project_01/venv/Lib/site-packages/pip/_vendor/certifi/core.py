504f2b2b24c7831fbcd0d5763c99335a704c1b71
kard sacifi <kk20050821@163.com>
2020-02-26 18:19:50 +0800

    2020 2/26 18:18 update python project 更新python工程文件

--- python_flask_02/project_01/venv/Lib/site-packages/pip/_vendor/certifi/core.py
+++ python_flask_02/project_01/venv/Lib/site-packages/pip/_vendor/certifi/core.py
@@ -1,15 +1,3 @@
-# -*- coding: utf-8 -*-
-
-"""
-certifi.py
-~~~~~~~~~~
-
-This module returns the installation location of cacert.pem.
-"""
-import os
-
-
-def where():
-    f = os.path.dirname(__file__)
-
-    return os.path.join(f, 'cacert.pem')
+version https://git-lfs.github.com/spec/v1
+oid sha256:12e15cd81b13a06e4ed7eaacc780528d0d6bd7eed646d1fcedbd567e564e5a12
+size 218
