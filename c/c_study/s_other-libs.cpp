43d4857a396c25abd598242fb1cd6f7d92e3df88
kard sacifi <kk20050821@163.com>
2020-03-18 11:50:42 +0800

    2020 3/18 11:50  update 完结c语言程序设计现代方法

--- c/c_study/s_other-libs.cpp
+++ c/c_study/s_other-libs.cpp
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 int max_int(int n ,...){//n为后面参数的数量 ... 表示后面可以跟其他可变数量的参数
     va_list ap;//这样可以强制函数访问到 n 后面的实参们
     printf(" n-> %d ",n);
@@ -36,8 +37,14 @@ struct city_info{
 };
 
 int compare_city(const void *key_ptr,const void *elment_ptr){
-    printf("\n run %s %s ",(char*)key_ptr,(char*)((city_info*)elment_ptr)->city);
+    printf("\n run Destination  %s find-> %s the answer %d ",(char*)key_ptr,(char*)((city_info*)elment_ptr)->city,strcmp((char*)key_ptr,((city_info*)elment_ptr)->city));
     return strcmp((char*)key_ptr,((city_info*)elment_ptr)->city);
+}
+
+int cmpfunc(const void * a, const void * b)
+{
+    printf("\n  测试 %d %d",*(int*)a,*(int*)b);
+   return ( *(int*)a - *(int*)b );
 }
 
 
@@ -57,22 +64,51 @@ int main(){
    printf("字符串值 = %s, 浮点值 = %f  %f \n", str, val,(val+20));
 
     printf("\n\n航班选择字符串查找....>");
-    char city_name[81]="linyi";
+    char *city_name="wuhan";
     city_info *ptr11;
     const struct city_info mileage[]=
     {
-        {"beijing",200},{"shanghai",300},
-        {"linyi",400},{"wuhan",500}
+        {"beijing",200},//这个是二分查找的得从小到大排序 按字母顺序从小到大排
+        {"linyi",400},
+        {"shanghai",300},
+        {"wuhan",500},
+        
+        
+        
     };
-                                                    //这里是元素的数量
-    ptr11=(city_info *)bsearch(city_name,mileage,sizeof(mileage)/sizeof(mileage[0]),
+
+    int values[] = { 5, 20, 29, 32, 63 };
+    int *item;
+    int key = 32;
+    item = (int*) bsearch (&key, values, 5, sizeof (int), cmpfunc);
+
+   if( item != NULL ) 
+   {
+      printf("Found item = %d\n", *item);
+   }
+   else 
+   {
+      printf("Item = %d could not be found\n", *item);
+   }
+
+
+
+
+
+    printf("\n  样本数量 %d - %d %d %d %d",sizeof(mileage)/sizeof(mileage[0]),
+    sizeof(mileage[0]),sizeof(mileage[1]),sizeof(mileage[2]),sizeof(mileage[3]));                                                //这里是元素的数量
+    ptr11=(city_info *)bsearch(city_name
+                                ,mileage
+                                ,sizeof(mileage)/sizeof(mileage[0]),
                     sizeof(mileage[0]),compare_city );//问题
                     //每个元素的大小     //这里指向比较函数
+    printf("\n answer %d",ptr11);
+
     if(ptr11 !=NULL){
         printf("%s,%d",ptr11->city,ptr11->miles);
     }else
     {
-        printf("no-> %s",city_name);
+        printf("\n no-> %s",city_name);
     }
     
 
