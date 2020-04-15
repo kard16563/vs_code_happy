339a071c2df08e56e48959446348b1f6d6e52885
kard sacifi <kk20050821@163.com>
2020-04-15 23:14:57 +0800

    2020 4/15  update assembly language(汇编语言)-0.1

--- c/c_study/s_hello.s
+++ c/c_study/s_hello.s
@@ -1,9 +1,10 @@
-.section .data
+.section .data#分段数据
 
-.section .text
-.globl _start
-_start:
-movl $1,%eax
+.section .text#分段 文本保存可读
+.globl _start#共链接使用
+_start:#入口
+movl $1,%eax#把 long 型1放入 寄存器 eax中
 movl $4,%ebx
-int $0x80
+int $0x80#int 进入内核 软中断 0x80 是系统调用
 #汇编语言 E:\the_c_of_vs_code\c\c_study\s_hello.s 
+#这位是在乌班图下的语法 win下的不是
