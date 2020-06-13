029811e611e82d8bf0e46e61fdc53a5a91ebaa43
kard sacifi <kk20050821@163.com>
2020-06-13 22:50:51 +0800

    2020 6/13 22:50 update java web project -1 - manage sys of books -0.0.4

--- JAVA/WEB/project/book-manage-system-prj-of-java-web/src/test_dao/student.java
+++ JAVA/WEB/project/book-manage-system-prj-of-java-web/src/test_dao/student.java
@@ -1,10 +1,16 @@
 package test_dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class student extends person {
     private int score =0;
+    /**
+     * list  有序列表
+     * */
+    List<String> list1 = new ArrayList<>();
 
     public void  setScore(int num){
-
         this.score = num;
     }
 
@@ -18,7 +24,16 @@ public class student extends person {
         System.out.println("son`s project ");
     }
 
+    /**
+     * list  有序列表
+     * */
+    public void listAdd(String value){
+        this.list1.add(value);
+    }
 
+    public void listCheckSize(){
+        System.out.println("输出有序列表的数量"+this.list1.size());
+    }
 
 
 
