801c493b084a494c79bbcc5cd50522bcc155664b
kard sacifi <kk20050821@163.com>
2020-06-14 10:55:06 +0800

    2020 6/14 10:54 update java web project -1 - manage sys of books -0.0.4-a

--- JAVA/WEB/project/book-manage-system-prj-of-java-web/src/web/test_a.java
+++ JAVA/WEB/project/book-manage-system-prj-of-java-web/src/web/test_a.java
@@ -6,10 +6,23 @@ import test_dao.student;
 public class test_a {
     public static void main(String[] args){
         student a1 = new student("0.123",11);
+        a1.setAge(10);
+
+        student a2 = new student("0.00",25);
+
         Colora cc = Colora.GREEN;
         String ans=cc.getNam();
 
-        System.out.println("okk");
+        a1.listAdd("000");
+        a1.listAdd("111");
+        a1.listAdd("222");
+
+        a1.listCheckSize(1);
+        a1.loopList();
+        a1.listRemove(2);
+        a1.listChange();
+
+        System.out.println("okk-------->end");
         a1.print();
 
     }
