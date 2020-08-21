c999fd2a1fe1566fb81c3a3ae46ebfaa1dd89f80
Kard Sacifi-win <kk20050821@163.com>
2020-08-21 11:21:17 +0800

    2020 8/21 11:20  update c++ files

--- c/c++.cpp
+++ c/c++.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-using namespace std;
-int main()
-{  
-    cout << "Hello, world!" << endl;
-    getchar();
-    return 0;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:203df86f52378c957188aa4ad486c67c86966da3d56ff4199e0b82ec98490a0d
+size 130
