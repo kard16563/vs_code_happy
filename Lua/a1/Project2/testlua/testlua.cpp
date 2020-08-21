33ff0346846535583834432cf6b73ed6245719b6
Kard Sacifi-win <kk20050821@163.com>
2020-08-21 11:36:08 +0800

    2020 8/21  11 : 34 update Lua

---
+++ Lua/a1/Project2/testlua/testlua.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+using namespace std;
+#include <lua.hpp>
+
+
+int main(void)
+
+
+
+{
+
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+    luaL_dofile(L, "main.lua");
+    lua_close(L);
+    cin.get();
+  //n.get();
+    return 0;
+
+
+
+    return 0;
+
+
+
+}
